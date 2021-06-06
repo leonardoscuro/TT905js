8410f0c53528ff59722edc933650aafae1de107f
leonardoscuro <l178624@dac.unicamp.br>
2021-06-06 04:06:51 -0300

    commit3

--- index.js
+++ index.js
@@ -2,24 +2,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-// Permissões
 var cors = require('cors');
 app.use(cors());
 
-// Porta que eu estou ouvindo
 app.listen(process.env.PORT || 3000);
-
-app.get('/', 
-    function (req, res){    
-        res.send("Hello World");
-    }
-);
-
-app.get('/hello',
-function (req, res){    
-    res.send("Hello de Novo");
-    }
-)
 
 const mensagens = [
     "Elziele da Rocha", "Lucas Canova dos Santos", 0 
